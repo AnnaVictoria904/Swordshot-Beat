d553804e4f0a0d0e21e1c146167a0d216528a499
Victoria <victoriaeso904@gmail.com>
2024-02-01 10:46:13 +0100

    Start Menu Improvement

--- Assets/Scripts/PassNextScene.cs
+++ Assets/Scripts/PassNextScene.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class CubeScript : MonoBehaviour
 {
+    [SerializeField] private GameObject cube;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Auto Hand Player Container")
@@ -15,7 +16,16 @@ public class CubeScript : MonoBehaviour
 
     IEnumerator WaitAndLoadScene()
     {
-        yield return new WaitForSeconds(2); 
-        SceneManager.LoadScene("LevelGame"); 
+        /*yield return new WaitForSeconds(2); 
+        SceneManager.LoadScene("LevelGame");*/
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("LevelGame");
+    }
+    private void Update()
+    {
+        if (cube.GetComponent<SliceObject>().hasSliced)
+        {
+            StartCoroutine(WaitAndLoadScene());
+        }
     }
 }
